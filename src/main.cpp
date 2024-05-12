8d0a0130399bdfabe700f346e50d714e3e4f06a7
Feruz <feruztursunovv@gmail.com>
2024-05-12 23:30:07 +0600

    Rewrite code structure. Using struct instead of class

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,26 @@
-#include <nlohmann/json.hpp>
 #include <iostream>
 #include <fstream>
 #include<string>
 #include<vector>
 #include<map>
 #define push_back pb
-using json = nlohmann::json;
-int main() {
+
+struct Book{
+    std:: string title;
+    std:: string author;
+    int year;
+    std:: string synopsis;
+};
+
+class Library {
+    private:
+        std:: vector<Book> books;
+
+    public:
+        void addBook(const Book& book){
+            books.push_back(book);
+            std:: cout<<"Book added successfully." << std:: endl;
+        }
     
+
 }
